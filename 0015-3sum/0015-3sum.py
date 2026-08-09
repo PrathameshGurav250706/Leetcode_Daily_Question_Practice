988f2edbd099698d30c7b4f544424753245e1682
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-08-09 11:43:41 +0530

    Time: 774 ms (35.53%), Space: 18.9 MB (25.49%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,36 @@
 class Solution(object):
     def threeSum(self, nums):
         nums.sort()
-        data=[]
-        n=len(nums)
-        for i in range(n-2):
-            if i>0 and nums[i]==nums[i-1]:
+        data = []
+        n = len(nums)
+
+        for i in range(n - 2):
+
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            l=i+1
-            r=n-1
 
-            while l<r:
-                if nums[i]+nums[l]+nums[r]==0:
-                    data.append([nums[i],nums[l],nums[r]])
+            l = i + 1
+            r = n - 1
 
-                    while l<r and nums[l]==nums[l+1]:
-                        l+=1
-                    while l<r and nums[r]==nums[r-1]:
-                        r-=1
-                    l+=1
-                    r-=1
-                elif nums[i]+nums[l]+nums[r]<0:
-                    l+=1
+            while l < r:
+                add = nums[i] + nums[l] + nums[r]
+
+                if add == 0:
+                    data.append([nums[i], nums[l], nums[r]])
+
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+
+                    l += 1
+                    r -= 1
+
+                elif add < 0:
+                    l += 1
+
                 else:
-                    r-=1
-        return data
+                    r -= 1
 
+        return data

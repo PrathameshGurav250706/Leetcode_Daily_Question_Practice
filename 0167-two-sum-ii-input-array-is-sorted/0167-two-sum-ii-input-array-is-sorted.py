26034ed862ea83ff6bc6431cb37f92e9d99f83c0
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-08-09 10:24:00 +0530

    Time: 11 ms (11.92%), Space: 12.7 MB (83.4%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,13 +5,12 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        left=0
-        right=len(numbers)-1
-
-        while left<right:
-            if numbers[left]+numbers[right]==target:
-                return[left+1,right+1]
-            elif numbers[left]+numbers[right]>target:
-                right-=1
+        l=0
+        r=len(numbers)-1
+        while l<r:
+            if numbers[l]+numbers[r]==target:
+                return[l+1,r+1]
+            elif numbers[l]+numbers[r]>target:
+                r-=1
             else:
-                left+=1
+                l+=1

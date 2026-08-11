681473f5a159be8b67c6b65c3a0ae6dc26a4ad55
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-08-11 11:21:23 +0530

    Time: 3 ms (85.36%), Space: 13.3 MB (57.42%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -4,21 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n=len(nums)-1
-        prefix=[0]*len(nums)
-        prefix[0]=nums[0]
-        for i in range(1,len(nums)):
-            prefix[i]=nums[i]+prefix[i-1]
-         
-        for j in range(len(nums)):
-            if j==0:
-                if 0==prefix[n]-prefix[0]:
-                    return 0
-            elif  prefix[j-1]==prefix[n]-prefix[j]:
-                return j
-        else:
-            return -1
+        total = sum(nums)
+        left_sum = 0
 
-                
+        for i in range(len(nums)):
+            right_sum = total - left_sum - nums[i]
 
-        
+            if left_sum == right_sum:
+                return i
+
+            left_sum += nums[i]
+
+        return -1

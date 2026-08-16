5acac5e114e4f5eab0f7730e27ada25f512e2e4c
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-08-16 11:56:16 +0530

    Time: 4 ms (98.17%), Space: 13.1 MB (55.1%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        prefix=1
+        preffix=1
         suffix=1
         ans=float('-inf')
         for i in range(len(nums)):
-            if prefix==0:
-                prefix=1
+            if preffix==0:
+                preffix=1
             if suffix==0:
                 suffix=1
-            prefix*=nums[i]
-            suffix*=nums[len(nums)-1-i]
-            ans=max(prefix,suffix,ans)
+            preffix*=nums[i]
+            suffix*=nums[len(nums)-i-1]
+            ans=max(ans,preffix,suffix)
         return ans

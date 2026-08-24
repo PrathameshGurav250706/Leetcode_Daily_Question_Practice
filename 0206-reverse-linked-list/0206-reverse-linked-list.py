9bdb766833d308d7ccbe015fc1446cb999b989a7
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-08-24 17:49:12 +0530

    Time: 0 ms (100%), Space: 14.3 MB (82.2%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -9,12 +9,12 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        prev = None
-        curr = head 
-        while curr :
-            nex = curr.next
-            curr.next = prev
-            prev = curr
-            curr= nex
+        prev=None
+        curr=head
+        while curr:
+            nex=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nex
         return prev
             

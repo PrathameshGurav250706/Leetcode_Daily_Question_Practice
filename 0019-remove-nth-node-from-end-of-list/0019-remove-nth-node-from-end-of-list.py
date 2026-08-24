bdc25d94ca975554e4438c7d0e745c954d00aa9c
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-08-24 17:35:22 +0530

    Time: 0 ms (100%), Space: 12.3 MB (94.84%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -17,10 +17,9 @@ class Solution(object):
 
         for i in range(n+1):
             fast=fast.next
-        
+
         while fast:
             slow=slow.next
             fast=fast.next
-        
         slow.next=slow.next.next
         return dummy.next

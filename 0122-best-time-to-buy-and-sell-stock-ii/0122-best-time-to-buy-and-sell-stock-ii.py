6efb4103e8dc671864c7823d41f973132d0ee51a
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-02 17:44:15 +0530

    Time: 7 ms (17.92%), Space: 13.9 MB (37.37%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -4,6 +4,11 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        # Time Complexity-O(n)
+        # Space Complexity- O(1)
+
+        # Logic is -> profit is max where next price is grater than current price, Add all maxProfit 
+        
         maxProfit=0
         for i in range(1,len(prices)):
             if prices[i]>prices[i-1]:

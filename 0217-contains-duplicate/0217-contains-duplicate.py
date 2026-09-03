841b364bc4c08f9618132dfe332a09230b8af856
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-03 16:43:31 +0530

    Time: 30 ms (45.01%), Space: 26 MB (23.55%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: bool
         """
         # Time complexity: O(n)
-        # Space complexity: O(n)
+        # Space complexity: O(n) in the worst case, since in the case with no duplicates the entire set seen will store all elements.
         seen=set()
         for i in nums:
             if i in seen:

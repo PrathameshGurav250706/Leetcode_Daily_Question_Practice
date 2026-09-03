f9b307935a545d3fea56723425b3146aa3cd617b
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-03 16:59:04 +0530

    Time: 0 ms (100%), Space: 13.9 MB (79.91%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        for i in nums:
-            if nums.count(i)==1:
-                return i
+        # # Time complexity: O(n^2)
+        # # Space complexity: O(1)
+        # for i in nums:
+        #     if nums.count(i)==1:
+        #         return i
         
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        d=0
+        for i in nums:
+            d=i^d           #XOR 
+
+        return d

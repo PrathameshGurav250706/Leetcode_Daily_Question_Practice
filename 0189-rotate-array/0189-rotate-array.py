5edeb0e84a5bbcfd6a1e5d037c22c6c3b65aeae0
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-03 16:38:07 +0530

    Time: 16 ms (28.62%), Space: 28.1 MB (10.6%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -6,21 +6,24 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
-        n=len(nums)
-        def rotate(start,end):
-            while start<=end:
-                nums[start],nums[end]=nums[end],nums[start]
-                start+=1
-                end-=1
+        # n=len(nums)
+        # def rotate(start,end):
+        #     while start<=end:
+        #         nums[start],nums[end]=nums[end],nums[start]
+        #         start+=1
+        #         end-=1
         
-        if k%n==0:
-            return
-        k=k%n
-        rotate(0,n-1)       #first rotate all list
-        rotate(0,k-1)        #rotate first k elements
-        rotate(k,n-1)       #rotate remaning elements
+        # if k%n==0:
+        #     return
+        # k=k%n
+        # rotate(0,n-1)       #first rotate all list
+        # rotate(0,k-1)        #rotate first k elements
+        # rotate(k,n-1)       #rotate remaning elements
 
-        
+        k=k%len(nums)
+        nums.reverse()
+        nums[k:]=reversed(nums[k:])
+        nums[:k]=reversed(nums[:k])
 
             
         

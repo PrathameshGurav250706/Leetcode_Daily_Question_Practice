dc0f538e1cb9b410d8eecf8a6c1a517670325987
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-05 15:01:14 +0530

    Time: 7 ms (56.21%), Space: 13.5 MB (50.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,14 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # i=0
-        # for j in range(len(nums)):
-        #     if nums[j]!=0:
-        #         nums[i],nums[j]=nums[j],nums[i]
-        #         i+=1
+        i=0
+        for j in range(len(nums)):
+            if nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+        # Time complexity: O(n)
+        # Space complexity: O(1)
 
-        for k in nums:
-            if k==0:
-                nums.remove(0)
-                nums.append(0)
+
+        # for k in nums:
+        #     if k==0:
+        #         nums.remove(0)
+        #         nums.append(0)
+        # time complexity degrades to O(n^2) in the worst case.
+        # Space complexity: O(1)
             

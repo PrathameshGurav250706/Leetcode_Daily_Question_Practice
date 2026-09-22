586e1cc29f631ef4166850a64e5055a2db65773c
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-22 18:57:26 +0530

    Time: 4 ms (35.21%), Space: 13 MB (68.3%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -4,20 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        left=0
-        right=len(nums)-1
+        # left=0
+        # right=len(nums)-1
 
-        while left<right:
-            if nums[left]%2!=0 and nums[right]%2==0:
-                nums[left],nums[right]=nums[right],nums[left]
+        # while left<right:
+        #     if nums[left]%2!=0 and nums[right]%2==0:
+        #         nums[left],nums[right]=nums[right],nums[left]
 
-            if nums[left]%2==0:
-                left+=1
-                
-            if nums[right]%2!=0:
-                right-=1
-        return nums
+        #     if nums[left]%2==0:
+        #         left+=1
+
+        #     if nums[right]%2!=0:
+        #         right-=1
+        # return nums
 
         # Time complexity: O(n) because each element is inspected a constant number of times as the pointers move toward each other. In the worst case, each element is swapped at most once, and the loop runs about n iterations.
 
         # Space complexity: O(1) extra space, besides the input array, since it uses a constant number of additional variables (left, right). The operation is in-place.
+
+        start=0
+        for i in range(len(nums)):
+            if nums[i]%2==0:
+                nums[start],nums[i]=nums[i],nums[start]
+                start+=1
+        return nums

46db4fe4beb371dac69f741239a62747dc07190b
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-24 17:09:02 +0530

    Time: 8 ms (37.68%), Space: 12.3 MB (61.59%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -4,7 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # for i in range(len(nums)):
+        #     if i==sum([int(k) for k in str(nums[i])]):
+        #         return i
+        # return -1
+
         for i in range(len(nums)):
-            if i==sum([int(k) for k in str(nums[i])]):
+            count=0
+            while nums[i]>0:
+                digit=nums[i]%10
+                count+=digit
+                nums[i]=nums[i]//10
+            if i==count:
                 return i
         return -1

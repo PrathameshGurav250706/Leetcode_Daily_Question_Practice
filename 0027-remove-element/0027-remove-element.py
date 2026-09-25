6f0c6d01c2d5dd65c282de0903ec75a65ba024c2
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-25 13:07:09 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.73%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        left=0
+        for right in range(len(nums)):
+            if nums[right]!=val:
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+        return left
+
+
+        

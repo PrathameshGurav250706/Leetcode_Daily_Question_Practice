77b32d136919aa0e8cd1bb950a8dd90f5f7b2ea4
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-26 17:54:36 +0530

    Time: 7 ms (54.45%), Space: 13.2 MB (88.2%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -10,5 +10,9 @@ class Solution(object):
             right=total-left-nums[i]
             if left==right:
                 return i
+            
             left=left+nums[i]
         return -1
+        # Time complexity: O(n), where n is the length of nums. The loop traverses the array once, and each iteration does O(1) work (constant-time arithmetic).
+
+        # Space complexity: O(1), aside from a few scalar variables (left, total, right). No extra data structures are used.

10df75165b460887d6cc6b0c3d63e11175b94ed6
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-26 18:16:18 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.43%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -6,13 +6,21 @@ class Solution(object):
         """
         # Time complexity: O(n),
         # Space complexity: O(n)
-        new=0
-        for i in digits:
-            new=new*10+i
-        new=new+1
-        seen=[]
-        for j in str(new):
-            seen.append(int(j))
-        return seen    
+        # new=0
+        # for i in digits:
+        #     new=new*10+i
+        # new=new+1
+        # seen=[]
+        # for j in str(new):
+        #     seen.append(int(j))
+        # return seen    
 
-       
+        for i in range(-1,-(len(digits)+1),-1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            else:
+                digits[i]=0
+        return [1]+digits
+    #   Time complexity: O(n) 
+    #   Space complexity: O(1) 

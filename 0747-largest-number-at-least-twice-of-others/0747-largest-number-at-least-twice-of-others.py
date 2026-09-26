f0766a7e119a34906638567fe3f25efcfdbcfc3b
PrathameshGurav250706 <prathameshgurav250706@gmail.com>
2026-09-26 18:00:40 +0530

    Time: 0 ms (100%), Space: 12.3 MB (91.03%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.py
@@ -9,4 +9,4 @@ class Solution(object):
             return nums.index(new[-1])
         return -1
 
-        
+        # This code sorts the input list, which takes O(n log n) time and O(n) extra space for the sort.
